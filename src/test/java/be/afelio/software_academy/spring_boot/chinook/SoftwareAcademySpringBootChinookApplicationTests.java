4344727764eb300dba1623f92a9c327d3e09b33b
salvatrice <salvatrice.montalbano@gmail.com>
2019-10-28 16:26:42 +0100

    test contextLoads

--- src/test/java/be/afelio/software_academy/spring_boot/chinook/SoftwareAcademySpringBootChinookApplicationTests.java
+++ src/test/java/be/afelio/software_academy/spring_boot/chinook/SoftwareAcademySpringBootChinookApplicationTests.java
@@ -1,6 +1,6 @@
 package be.afelio.software_academy.spring_boot.chinook;
 
-import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SoftwareAcademySpringBootChinookApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
 
 }
